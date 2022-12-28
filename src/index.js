1598cdf47433ed1cbd699fd8689e62621486d1c1
Kasope Johnson <kasopej@gmail.com>
2022-12-28 16:21:30 +0100

    creates html list with createElement method

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const mainHeading = React.createElement(
+  "ol",
+  null,
+  Array(1, 2, 3).map((index) =>
+    React.createElement("li", { key: index }, `Item ${index}`)
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root") ?? document.body).render(
+  mainHeading
+);
