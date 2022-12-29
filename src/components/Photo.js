7ad58dc9360e8253ef9cb09debf1779789b91586
Kasope Johnson <kasopej@gmail.com>
2022-12-29 01:43:58 +0100

    adds photo caption & delete button

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,6 +2,21 @@ import { Component } from "react";
 
 export default class Photo extends Component {
   render() {
-    return <figure className="figure"></figure>;
+    const { photo } = this.props;
+    return (
+      <figure className="figure">
+        <img
+          className="photo"
+          src={photo.imageLink}
+          alt={photo.description}
+        ></img>
+        <figcaption>
+          <p>{photo.description}</p>
+        </figcaption>
+        <div className="button-container">
+          <button className="remove-button">Remove</button>
+        </div>
+      </figure>
+    );
   }
 }
