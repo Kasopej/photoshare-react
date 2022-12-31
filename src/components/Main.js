387ad5e6e205f7f6911f252ba77fb82e71ffe2cf
Kasope Johnson <kasopej@gmail.com>
2022-12-31 18:20:25 +0100

    uses lifecy;cle methods for better understanding

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,31 +5,32 @@ import PhotoWall from "./PhotoWall";
 export default class MainApp extends Component {
   constructor(props) {
     super(props);
+    console.log("create");
     this.state = {
-      posts: [
-        {
-          id: "0",
-          description: "beautiful landscape",
-          imageLink:
-            "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-            "3919321_1443393332_n.jpg",
-        },
-        {
-          id: "1",
-          description: "Aliens???",
-          imageLink:
-            "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg",
-        },
-        {
-          id: "2",
-          description: "On a vacation!",
-          imageLink:
-            "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg",
-        },
-      ],
+      posts: [],
     };
     this.removePost = this.removePost.bind(this);
     this.addPost = this.addPost.bind(this);
+  }
+  componentWillMount(nextProps, nextState) {
+    console.log("before mount", { nextProps, nextState });
+  }
+  componentDidMount(prevProps, prevState) {
+    console.log("mount", { prevProps, prevState });
+    setTimeout(async () => {
+      this.setState({
+        posts: await getPhotos(),
+      });
+    }, 1500);
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log("before update", { nextProps, nextState });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("updated", { prevProps, prevState });
+  }
+  componentWillUnmount(nextProps, nextState) {
+    console.log("destroying", { nextProps, nextState });
   }
   addPost(post) {
     this.state.posts.push(post);
@@ -43,6 +44,7 @@ export default class MainApp extends Component {
     }));
   }
   render() {
+    console.log("render");
     return (
       <main>
         <Title title="PhotoWall"></Title>
@@ -53,4 +55,27 @@ export default class MainApp extends Component {
       </main>
     );
   }
+}
+
+function getPhotos() {
+  return Promise.resolve([
+    {
+      id: "0",
+      description: "beautiful landscape",
+      imageLink:
+        "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+        "3919321_1443393332_n.jpg",
+    },
+    {
+      id: "1",
+      description: "Aliens???",
+      imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg",
+    },
+    {
+      id: "2",
+      description: "On a vacation!",
+      imageLink:
+        "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg",
+    },
+  ]);
 }
