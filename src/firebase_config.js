b104034c3505ac4527982f48aecf3be10dc3b97d
Kasope Johnson <kasopej@gmail.com>
2023-01-06 21:51:34 +0100

    moves firebase config to different module for reuse

---
+++ src/firebase_config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+const database = getDatabase(installFirebase());
+
+function installFirebase() {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBLWYqTkcY0wJ6DX44N8cM2KcI7xENILnY",
+    authDomain: "photoshare-5e927.firebaseapp.com",
+    projectId: "photoshare-5e927",
+    storageBucket: "photoshare-5e927.appspot.com",
+    messagingSenderId: "1066131141429",
+    appId: "1:1066131141429:web:49130d1264be292a06527c",
+    measurementId: "G-Q650DL27LP",
+  };
+
+  // Initialize Firebase app
+  return initializeApp(firebaseConfig);
+}
+
+export default database;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import ReactDOM from "react-dom/client";
 import MainApp from "./components/Main";
 import "./assets/css/main.css";
 import { BrowserRouter } from "react-router-dom";
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./redux/reducer";
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-installFirebase();
+import thunk from "redux-thunk";
+import "./firebase_config";
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 );
 
 ReactDOM.createRoot(document.getElementById("root") ?? document.body).render(
@@ -24,19 +21,3 @@ ReactDOM.createRoot(document.getElementById("root") ?? document.body).render(
     </BrowserRouter>
   </Provider>
 );
-
-function installFirebase() {
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBLWYqTkcY0wJ6DX44N8cM2KcI7xENILnY",
-    authDomain: "photoshare-5e927.firebaseapp.com",
-    projectId: "photoshare-5e927",
-    storageBucket: "photoshare-5e927.appspot.com",
-    messagingSenderId: "1066131141429",
-    appId: "1:1066131141429:web:49130d1264be292a06527c",
-    measurementId: "G-Q650DL27LP",
-  };
-
-  // Initialize Firebase
-  initializeApp(firebaseConfig);
-}
