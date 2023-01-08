05a68e032f0b1bcc8445fa1149e58fb5955e6956
Kasope Johnson <kasopej@gmail.com>
2023-01-08 02:12:57 +0100

    removes unused proptype function

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { startAddingPost } from "../redux/actions";
 import withNavigationHOC from "./utilities/WithNavigationHOC";
